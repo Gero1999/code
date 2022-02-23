01f3dc7d54b5555dd6fcdf3562f7fcc77814099b
Gerardo <68994823+Gero1999@users.noreply.github.com>
2022-02-23 13:38:03 +0100

    Update resume.py

--- streamlit/resume/resume.py
+++ streamlit/resume/resume.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-with open("style.css") as f:
+with open("https://raw.githubusercontent.com/Gero1999/code/main/streamlit/resume/style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-
 
 # Title/Header
 st.write('''
