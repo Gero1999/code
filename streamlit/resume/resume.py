2c80f7eb39353369ab3c5be57494a407fe51c5a4
Gerardo <68994823+Gero1999@users.noreply.github.com>
2022-02-24 18:28:47 +0100

    Update resume.py

--- streamlit/resume/resume.py
+++ streamlit/resume/resume.py
@@ -10,7 +10,7 @@ st.write('''
 #### *Resume & Qualifications as a bioinformatician* 
 ''')
 
-image = Image.open('streamlit/resume/image.png')
+image = Image.open('streamlit/resume/Imagen3.png')
 st.image(image, width=700)
 
 st.markdown('## Summary', unsafe_allow_html=True)
