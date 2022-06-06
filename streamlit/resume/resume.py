47384ad4e456109c273aa64061be87d779ab1e43
Gerardo <68994823+Gero1999@users.noreply.github.com>
2022-06-06 02:37:50 +0200

    Update resume.py

--- streamlit/resume/resume.py
+++ streamlit/resume/resume.py
@@ -183,7 +183,10 @@ txt2('ORCID', 'https://orcid.org/my-orcid?orcid=0000-0003-1413-0060')
 txt2('Email', 'gerardo.jrac@gmail.com')
 
 #####################
+st.markdown('''
 ## Availability
+---
+''')
 st.markdown('<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23A79B8E&ctz=Europe%2FCopenhagen&mode=WEEK&showDate=0&showPrint=0&showTabs=1&showCalendars=1&hl=en_GB&showNav=1&showTitle=1&title&src=Z2VyYXJkby5qcmFjQGdtYWlsLmNvbQ&color=%23039BE5" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>',
            unsafe_allow_html=True)
 
