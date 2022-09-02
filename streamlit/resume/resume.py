f9894257e2e07bce29493b749b9f8ae037e15a2b
Gerardo <68994823+Gero1999@users.noreply.github.com>
2022-09-02 17:50:49 +0200

    Update resume.py

--- streamlit/resume/resume.py
+++ streamlit/resume/resume.py
@@ -204,9 +204,8 @@ st.markdown('''
 ## Availability
 ---
 ''')
-st.markdown('<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23A79B8E&ctz=Europe%2FCopenhagen&mode=WEEK&showDate=0&showPrint=0&showTabs=1&showCalendars=1&hl=en_GB&showNav=1&showTitle=1&title&src=Z2VyYXJkby5qcmFjQGdtYWlsLmNvbQ&color=%23039BE5" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>',
+st.markdown('<iframe src="https://calendar.google.com/calendar/embed?src=og3sp800lruhb5q03g35hqcqdsnhov5j%40import.calendar.google.com&ctz=Europe%2FBerlin"  style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>',
            unsafe_allow_html=True)
-
 
 ####################
 st.markdown(''' '''); st.markdown(''' ''')
