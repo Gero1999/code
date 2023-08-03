698b94dff25d02246cc2225ea71332f2c289cb65
Gerardo Rodríguez <68994823+Gero1999@users.noreply.github.com>
2023-08-03 18:02:41 +0200

    Update and rename nussi.py to nussinov.py

--- Python/Nussinov/nussinov.py
+++ Python/Nussinov/nussinov.py
@@ -4,11 +4,11 @@ def nussinov(seq, scores={'AU': 1, 'UA': 1, 'GU': 1, 'UG': 1, 'GC': 1, 'CG': 1},
     using the scoring system specified in a explicit dictionary (scores) and a restriction
     imposing a minimum loop size (h) for the output represented using dot-bracket notation.'''
 
-    # sequence length and initial matrix
+    # Sequence length and initial matrix definition
     l = len(seq)
     m = [[0 for i in range(l)] for j in range(l)]
 
-    # fill scoring matrix
+    # Fill scoring matrix
     for j0 in range(h + 1, l):  # the diagonal of the matrix to loop over
         for i in range(0, l - j0):  # the entry on the diagonal to fill
             j = i + j0
@@ -26,7 +26,7 @@ def nussinov(seq, scores={'AU': 1, 'UA': 1, 'GU': 1, 'UG': 1, 'GC': 1, 'CG': 1},
                 if m[i][k] + m[k + 1][j] > m[i][j]:
                     m[i][j] = m[i][k] + m[k + 1][j]
 
-    # backtracking - simple implementation of pseudo-code given in lecture
+    # Backtracking to keep only base pairs that maximize the score
     structure = ['.' for i in range(l)]
     stack = []
     stack.append((0, l - 1))
@@ -53,37 +53,13 @@ def nussinov(seq, scores={'AU': 1, 'UA': 1, 'GU': 1, 'UG': 1, 'GC': 1, 'CG': 1},
      # output
     return ''.join(structure)
 
-# Sequences to compute
+
+
+## Example
+
+# Sequence to compute
 seq1 = 'UACUUAAGCCGACAUGAACGGUGACACCUAGCCAAUGUUGAGUCUGAAGAGAAGAACUUCAAGUACAGUAAAUGGUAGGUUAU'
-seq2 = 'UUCCUUUGGGCCCUGUUGGGGCCCAAAGGGGUUCUUCAAAACGCGCUGCCCUUCUUCUGGGCAGCGCGUUUU'
 
 # Do the Nussinov implementation to the sequences and find the loops using a regular expression
 import re
 loops_s1 = [m.span() for m in re.finditer('[\(\)]\.{3,}[\(\)]', nussinov(seq=seq1, h=3))]
-loops_s2 = [m.span() for m in re.finditer('[\(\)]\.{3,}[\(\)]', nussinov(seq=seq2, h=3))]
-
-# All possible combinations between loops is performed
-combinations = [(x, y) for x in loops_s1 for y in loops_s2]
-
-# Represent the combinations in two dataframes, one storing the dot-bracket representation and the other one the max points
-import pandas as pd
-df_str = pd.DataFrame(columns=loops_s1, index=loops_s2)
-df_pts = pd.DataFrame(columns=loops_s1, index=loops_s2)
-for comb in combinations:
-    # String sequences to compare
-    loop1 = seq1[comb[0][0]+1:comb[0][1]-1]
-    loop2 = seq2[comb[1][0]+1:comb[1][1]-1]
-
-    # String structures predicted and nr of base pairs (points)
-    max_l = max(len(loop1), len(loop2))
-    structure = nussinov(loop1 + 'X'*max_l + loop2, h=max_l)
-    structure = structure[0:len(loop1)]+'  '+structure[len(loop1)+max_l:len(loop1)+max_l+len(loop2)]
-    points_str = len(structure.split('('))-1
-
-    # Dataframe summaries of the representations and the points
-    df_str[comb[0]][comb[1]] = structure
-    df_pts[comb[0]][comb[1]] = points_str
-
-# Print results
-print(df_str)   # Represent (one of) the potential interaction between loops (columns: seq1 loop indices, index: seq2 loop indices)
-print(df_pts)   # Represent the maximum base pair number that can occur for each loop interaction
